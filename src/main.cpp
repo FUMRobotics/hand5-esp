e26d2ea7aca61af8a79eb35f99f61f49a8510119
MHTaji <mh.taji2047@gmail.com>
2023-08-18 16:47:01 +0330

    add uart application + write get request for WIFI communication must be test wifi communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,102 @@
-#include "BluetoothSerial.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
-/* Check if Bluetooth configurations are enabled in the SDK */
-/* If not, then you have to recompile the SDK */
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
+const char *ssid = "HAND";
+const char *password = "FUM_HAND";
 
-BluetoothSerial SerialBT;
+// Your Domain name with URL path or IP address with path
+String serverName = "http://192.168.1.1/Send";
 
-void setup() {
+String ptr;
+StaticJsonDocument<200> doc;
+DeserializationError error;
+struct
+{
+  uint8_t Pinky;
+  uint8_t Ring;
+  uint8_t Middle;
+  uint8_t Index;
+  uint8_t Thumb;
+} HandFinger;
+void setup()
+{
+  Serial2.begin(115200);
   Serial.begin(115200);
-  /* If no name is given, default 'ESP32' is applied */
-  /* If you want to give your own name to ESP32 Bluetooth device, then */
-  /* specify the name as an argument SerialBT.begin("myESP32Bluetooth"); */
-  SerialBT.begin("ESP32_HAND_BLE");
-  Serial.println("Bluetooth Started! Ready to pair...");
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  if (Serial.available())
+void loop()
+{
+  if (Serial2.available())
   {
-    SerialBT.write(Serial.read());
+    ptr = Serial2.readStringUntil('\n');
+    Serial.print(ptr);
+    error = deserializeJson(doc, ptr);
+    HandFinger.Index = doc["Index"];
+    HandFinger.Middle = doc["Middle"];
+    HandFinger.Pinky = doc["Pinky"];
+    HandFinger.Ring = doc["Ring"];
+    HandFinger.Thumb = doc["Thumb"];
+    if (error)
+    {
+      Serial.print(F("\ndeserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+    }
+    //---------------------------- WIFI Communnication ---------------------------------------------
+    if (doc["Communication"] == "WIFI")
+    {
+      Serial.println("\n\nwifi start");
+      // Check WiFi connection status
+      if (WiFi.status() == WL_CONNECTED)
+      {
+        HTTPClient http;
+
+        String serverPath = serverName + "?ThumbValue=" + HandFinger.Thumb + "&MiddleValue=" + HandFinger.Middle + "&IndexValue=" + HandFinger.Index + "&RingValue=" + HandFinger.Ring + "&PinkyValue=" + HandFinger.Pinky;
+
+        // Your Domain name with URL path or IP address with path
+        http.begin(serverPath.c_str());
+
+        // If you need Node-RED/server authentication, insert user and password below
+        // http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+
+        // Send HTTP GET request
+        int httpResponseCode = http.GET();
+
+        if (httpResponseCode > 0)
+        {
+          Serial.print("HTTP Response code: ");
+          Serial.println(httpResponseCode);
+          String payload = http.getString();
+          Serial.println(payload);
+        }
+        else
+        {
+          Serial.print("Error code: ");
+          Serial.println(httpResponseCode);
+        }
+        // Free resources
+        http.end();
+      }
+      else
+      {
+        Serial.println("WiFi Disconnected");
+      }
+    }
+    //---------------------------- Bluetooth Communnication ---------------------------------------------
+    if (doc["Communication"] == "Bluetooth")
+    {
+      Serial.println("\n\nbluetooth start");
+    }
   }
-  if (SerialBT.available())
-  {
-    Serial.write(SerialBT.read());
-  }
-  
 }
